8fda880506a63272f260a4cc42667df1741bdebd
Ernani da Costa <ernanidacosta@gmail.com>
2019-12-15 11:12:14 -0300

    Create complex atribute

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 33):
+    def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
-        self.nome = None
+        self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Rose')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Ernani'
-    print(p.nome)
-    print(p.idade)
+    rose = Pessoa(nome='Rose')
+    ernani = Pessoa(rose, nome='Ernani')
+    print(Pessoa.cumprimentar(ernani))
+    print(id(rose))
+    print(ernani.cumprimentar())
+    print(ernani.nome)
+    print(ernani.idade)
+    for filho in ernani.filhos:
+        print(filho.nome)
