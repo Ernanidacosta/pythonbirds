89c5206ba2ea075fbd3596562062e9c9d940aa5d
Ernani da Costa <ernanidacosta@gmail.com>
2019-12-15 10:42:40 -0300

    Update on Class Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 33):
+        self.idade = idade
+        self.nome = None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Rose')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ernani'
+    print(p.nome)
+    print(p.idade)
