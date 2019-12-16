7ba65fa709825850a5a3b040c5b1d6cb5d1a9933
Ernani da Costa <ernanidacosta@gmail.com>
2019-12-16 15:00:07 -0300

    Created Class attribute olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos=2
+
     def __init__(self, *filhos, nome=None, idade=33):
         self.idade = idade
         self.nome = nome    
@@ -18,5 +20,15 @@ if __name__ == '__main__':
     print(ernani.idade)
     for filho in ernani.filhos:
         print(filho.nome)
+    ernani.sobrenome = 'Costa'
+    del ernani.filhos
+    ernani.olhos = 1
+    del ernani.olhos
     print(ernani.__dict__)
     print(rose.__dict__)
+    Pessoa.olhos=3
+    print(Pessoa.olhos)
+    print(ernani.olhos)
+    print(rose.olhos)
+    print(id(Pessoa.olhos), id(ernani.olhos), id(rose.olhos))
+
