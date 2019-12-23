f21b29f25779f79e6389cdce94b44539655f5714
Ernani da Costa <ernanidacosta@gmail.com>
2019-12-23 01:03:23 -0300

    Created some methods

--- oo/carro.py
+++ oo/carro.py
@@ -103,6 +103,30 @@ LESTE = 'Leste'
 OESTE = 'Oeste'
 
 
+class Carro():
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+
 class Direcao():
     rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
     rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
@@ -115,8 +139,6 @@ class Direcao():
 
     def girar_a_direita(self):
         self.valor = self.rotacao_a_direita_dct[self.valor]
-
-
 
 
 class Motor:
